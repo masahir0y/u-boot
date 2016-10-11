e2f9c61ad5ba835f1bb0f1ad802de6b1e5ddeee8
Masahiro Yamada <yamada.masahiro@socionext.com>
2016-10-11 21:28:41 +0900

    ARM: uniphier: support passing bl31_params to ATF
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- arch/arm/mach-uniphier/arm64/atf.c
+++ arch/arm/mach-uniphier/arm64/atf.c
@@ -13,10 +13,36 @@
 #define ARMTF_BASE		0x000c0000
 #define ARMTF_SIZE		0x00010000
 
+struct atf_param_header {
+	u8 type;		/* type of the structure */
+	u8 version;    /* version of this structure */
+	u16 size;      /* size of this structure in bytes */
+	u32 attr;      /* attributes: unused bits SBZ */
+};
+
+struct atf_image_info {
+	struct atf_param_header h;
+	unsigned long image_base;   /* physical address of base of image */
+	u32 image_size;    /* bytes read from image file */
+};
+
+struct atf_bl31_params {
+	struct atf_param_header h;
+	struct atf_image_info *bl31_image_info;
+	void *bl32_ep_info;
+	struct atf_image_info *bl32_image_info;
+	void *bl33_ep_info;
+	struct atf_image_info *bl33_image_info;
+};
+
+typedef void (*bl31_entry_t)(struct atf_bl31_params *bl31_params,
+			     void *plat_params);
+
 void spl_board_prepare_for_boot(void)
 {
-	typedef void (*bl31_entry_t)(void);
 	bl31_entry_t bl31_entry;
+	static struct atf_bl31_params bl31_params;
+	static struct atf_image_info bl33_image_info;
 
 	bl31_entry = (bl31_entry_t)ARMTF_LOAD_ADDR;
 
@@ -26,5 +52,8 @@ void spl_board_prepare_for_boot(void)
 
 	ISB;
 
-	bl31_entry();
+	bl33_image_info.image_base = CONFIG_SYS_TEXT_BASE;
+	bl31_params.bl33_image_info = &bl33_image_info;
+
+	bl31_entry(&bl31_params , NULL);
 }
