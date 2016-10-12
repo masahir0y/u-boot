d370c3e10ca5ba283fd9dfc23034e23d4dac875e
Masahiro Yamada <yamada.masahiro@socionext.com>
2016-10-12 15:27:07 +0900

    ARM: uniphier: support ATF load from eMMC
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- arch/arm/mach-uniphier/arm64/atf.c
+++ arch/arm/mach-uniphier/arm64/atf.c
@@ -6,12 +6,17 @@
  */
 
 #include <common.h>
+#include <spl.h>
 #include <linux/string.h>
 #include <asm/barriers.h>
 
+#include "../boot-mode/boot-device.h"
+
 #define ARMTF_LOAD_ADDR		0x80000000
-#define ARMTF_BASE		0x000d0000
 #define ARMTF_SIZE		0x00010000
+
+#define ARMTF_NOR_BASE		0x000d0000
+#define ARMTF_EMMC_SECTOR	0x500
 
 struct atf_param_header {
 	u8 type;		/* type of the structure */
@@ -35,6 +40,9 @@ struct atf_bl31_params {
 	struct atf_image_info *bl33_image_info;
 };
 
+int uniphier_spl_emmc_load_image(struct spl_image_info *spl_image,
+				 u32 dev_addr);
+
 typedef void (*bl31_entry_t)(struct atf_bl31_params *bl31_params,
 			     void *plat_params);
 
@@ -43,14 +51,28 @@ void spl_board_prepare_for_boot(void)
 	bl31_entry_t bl31_entry;
 	static struct atf_bl31_params bl31_params;
 	static struct atf_image_info bl33_image_info;
+	struct spl_image_info spl_image;
+	int ret;
 
-	bl31_entry = (bl31_entry_t)ARMTF_LOAD_ADDR;
+	spl_image.load_addr = ARMTF_LOAD_ADDR;
+	spl_image.size = ARMTF_SIZE;
 
-	memcpy((void *)ARMTF_LOAD_ADDR, (void *)ARMTF_BASE, ARMTF_SIZE);
+	switch (spl_boot_device_raw()) {
+	case BOOT_DEVICE_NOR:
+		memcpy((void *)(uintptr_t)spl_image.load_addr, (void *)ARMTF_NOR_BASE,
+		       spl_image.size);
+		break;
+	case BOOT_DEVICE_MMC1:
+		ret = uniphier_spl_emmc_load_image(&spl_image,
+						   ARMTF_EMMC_SECTOR);
+		if (ret)
+			return;
+		break;
+	default:
+		return;
+	}
 
-	printf("Jumping to BL31 %p\n", bl31_entry);
-
-	ISB;
+	bl31_entry = (bl31_entry_t)(uintptr_t)spl_image.load_addr;
 
 	bl33_image_info.image_base = CONFIG_SYS_TEXT_BASE;
 	bl31_params.bl33_image_info = &bl33_image_info;
